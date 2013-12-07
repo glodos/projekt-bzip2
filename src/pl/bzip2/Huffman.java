81d2db745150a47700096c63ab7b21fad6c04e9e
glodos <krzysztof.glodowski@gmail.com>
2013-12-07 16:55:21 +0100

    dodany zapis i odczyt drzewa z tablicy bajtów

--- src/pl/bzip2/Huffman.java
+++ src/pl/bzip2/Huffman.java
@@ -1,59 +1,91 @@
 package pl.bzip2;
 
+import java.nio.ByteBuffer;
 import java.util.PriorityQueue;
 
 public class Huffman {
-    private static char[] vector;
+    private byte[] vector;
+    private int nodeCount;
+    private Node treeRoot;
 
-    public Huffman(char [] vector) {
+    public Huffman(byte [] vector) {
         this.vector = vector;
     }
 
-    public static void encode() {
-        int [] charFreqs = new int[256];
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
+    public void encode(BitWriter writer) {
+        nodeCount = 0;
         // 0. Calculate chars frequencies
-        for(int i = 0; i < vector.length; i++)
-            charFreqs[vector[i]]++;
-
+        int[] symFreqs = calcFrequencies();
         // 1. Create a leaf node for each symbol and add it to the priority queue
-        for(int i = 0; i < 256; i++) {
-            if(charFreqs[i] > 0)
-                pq.add(new Node(i, freq[i], null, null));
+        PriorityQueue<Node> pq = fillQueueWithLeafNodes(symFreqs);
+        treeRoot = buildTree(pq);
+        
+    }
+    
+    private int[] calcFrequencies(){
+    	int [] symFreqs = new int[256];
+    	for(int i = 0; i < vector.length; i++)
+            symFreqs[vector[i]]++;
+    	return symFreqs;
+    }
+    
+    private PriorityQueue<Node> fillQueueWithLeafNodes(int[] symFreqs){
+    	PriorityQueue<Node> pq = new PriorityQueue<>();
+    	for(int i = 0; i < 256; i++) {
+            if(symFreqs[i] > 0){
+                pq.add(new Node((byte)i, symFreqs[i], null, null));
+                nodeCount++;
+            }
         }
-
-        // 2. While there is more than one node in the queue:
+    	return pq;
+    }
+    
+    private Node buildTree(PriorityQueue<Node> pq){
+    	// 2. While there is more than one node in the queue:
         while(pq.size() > 1) {
             // 2.1 Remove the two nodes of highest priority (lowest probability) from the queue
             Node left = pq.poll();
             Node right = pq.poll();
 
             // 2.2 Create a new internal node with these two nodes as children and with probability equal to the sum of the two nodes' probabilities.
-            Node merged = new Node('\0', left.freq+right.freq, left, right);
+            Node merged = new Node((byte)0, left.freq+right.freq, left, right);
 
             // 2.3 Add the new node to the queue.
             pq.add(merged);
+            nodeCount++;
         }
-
-
-
+        return pq.peek();
     }
+    
+    public byte[] getTree(){
+    	return flattenTree(treeRoot, nodeCount);
+    }
+    
+    private static byte[] flattenTree(Node root, int nodeCount){
+    	ByteBuffer buffer = ByteBuffer.allocate(nodeCount+1);
+    	root.writeValue(buffer, 1);
+    	return buffer.array();
+    }
+    
+    private static Node readTree(byte[] bytes){
+    	return Node.readNode(bytes, 1);
+    }
+    
 
     private static class Node implements Comparable<Node> {
-        char c;
+    	byte symbol;
         private final int freq;
         private Node left;
         private Node right;
 
-        private Node(char c, int freq, Node left, Node right) {
-            this.c = c;
+        private Node(byte symbol, int freq, Node left, Node right) {
+            this.symbol = symbol;
             this.freq = freq;
             this.left = left;
             this.right = right;
         }
 
-        private Boolean isLeaf() {
+        private boolean isLeaf() {
             assert (left == null && right == null) || (left != null && right != null);
             return (left == null && right == null);
         }
@@ -61,6 +93,21 @@ public class Huffman {
         @Override
         public int compareTo(Node o) {
             return this.freq - o.freq;
+        }
+        
+        public void writeValue(ByteBuffer b, int position){
+        	b.put(position, symbol);
+        	if(!isLeaf()){
+        		left.writeValue(b, position*2);
+        		right.writeValue(b, position*2+1);
+        	}
+        }
+        
+        public static Node readNode(byte[] buf, int position){
+        	if(position>buf.length)
+        		return null;
+        	else
+        		return new Node(buf[position-1], 0, readNode(buf, position*2), readNode(buf, position*2+1));
         }
     }
 }
